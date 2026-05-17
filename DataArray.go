9abb84aba44892bad2cbdff40525454ebc1367da
Mipan-Zuzu <kadekisaka00@gmail.com>
2026-05-17 23:35:41 +0800

    feat: arrat append, len make

--- DataArray.go
+++ DataArray.go
@@ -1,6 +1,10 @@
 package main
 import "fmt"
 
+var values = [3] int {
+	30, 40, 50,
+}
+
 func Arry () {
 	var names [3] string
 	names[0] = "mipan"
@@ -10,4 +14,23 @@ func Arry () {
 	for _, name := range names {
 		fmt.Println(name)
 	}
+
+	fmt.Println("ini values", values)
+	Slices()
+}
+
+func Slices () {
+	namess := [...]string{"mipan", "zuzu", "cuki", "liar", "rrampe", "Bawdewak"}
+	slice := namess[4:]
+
+	names2 := append(slice, "cukiliar")
+	names3 := len(names2)
+
+	names4 := make([]string, len(namess))
+	copy([6]string(names4),namess)
+
+	fmt.Println(slice[0])
+	fmt.Println(names2)
+	fmt.Println(names3)
+	fmt.Println(names4)
 }
